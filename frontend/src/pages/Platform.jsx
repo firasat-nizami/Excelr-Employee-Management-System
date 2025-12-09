1df4f78dbd6c1a1892db7187bf41e10ea5ff090a
Amit Das <d.amit.24@gmail.com>
2025-12-09 18:37:51 +0530

    original dashbaord file

--- frontend/src/pages/Platform.jsx
+++
@@ -1,8 +0,0 @@
-export default function Platform() {
-  return (
-    <div>
-      <h2>Platform Page</h2>
-      <p>Welcome to the Platform section of the Employee Management System.</p>
-    </div>
-  );
-}
