6f9f4985c568b63c3a93f37e6108cdc7542b7b2f
unknown <firasatali882@gmail.com>
2025-12-10 15:44:59 +0530

    deleted all fronted due to major error cause  by sweety

--- frontend/src/pages/PrivacyPolicy.jsx
+++
@@ -1,8 +0,0 @@
-export default function PrivacyPolicy() {
-  return (
-    <div>
-      <h2>Privacy Policy</h2>
-      <p>Your privacy is important to us.</p>
-    </div>
-  );
-}
--- frontend/src/pages/Terms.jsx
+++
@@ -1,8 +0,0 @@
-export default function Terms() {
-  return (
-    <div>
-      <h2>Terms & Conditions</h2>
-      <p>Please read the terms carefully before using our platform.</p>
-    </div>
-  );
-}
